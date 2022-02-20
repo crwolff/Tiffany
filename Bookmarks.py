706f8148854c47a789a38cde8c3766611d67036a
CRWolff <27215580+crwolff@users.noreply.github.com>
2022-02-20 16:00:01 -0700

    Move thumbnails into separate class

---
+++ Bookmarks.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QFileDialog, QMessageBox, QListWidget, QListWidgetItem
+from PyQt5.QtGui import QImage, QPixmap
+
+class Bookmarks(QListWidget):
+    def __init__(self, parent=None):
+        QListWidget.__init__(self, parent)
+
+    def readFiles(self):
+        # Determine button pressed
+        whom = self.sender().objectName()
+        if whom == "insertAct":
+            txt = 'Insert Files'
+        elif whom == "replaceAct":
+            txt = 'Replace Files'
+        else:
+            txt = 'Open Files'
+
+        # Popup file dialog
+        options = QFileDialog.Options()
+        fileNames,_ = QFileDialog.getOpenFileNames(self, txt, '', 'Images (*.png)', options=options)
+        if len(fileNames) == 0:
+            return
+
+        # For replace or insert, get list of items
+        rows = []
+        if (whom == 'insertAct') or (whom == 'replaceAct'):
+            for x in self.selectedItems():
+                rows.append(int(x.text()) - 1)
+            rows.sort()
+            if len(rows) == 0:
+                QMessageBox.information(self, txt, "Insertion point must be selected")
+                return
+
+#        # Add progress to status bar
+#        self.statusLabel.setText("Reading...")
+#        pbar = QProgressBar()
+#        pbar.setRange(0, len(fileNames))
+#        pbar.setValue(0)
+#        self.statusbar.addWidget(pbar)
+
+        # Open each file in turn and add to listWidget
+        for x in fileNames:
+            image = QImage(x)
+            if image.isNull():
+                QMessageBox.information(self, "Tiffany", "Cannot load %s." % x)
+            else:
+                # Convert from indexed to grayscale
+                if image.format() == QImage.Format_Indexed8:
+                    image = image.convertToFormat(QImage.Format_Grayscale8)
+
+                # Make icon
+                qimg = image.scaled(100, 100, QtCore.Qt.KeepAspectRatio, QtCore.Qt.SmoothTransformation)
+                qpix = QPixmap.fromImage(qimg)
+                icon = QtGui.QIcon(qpix)
+
+                # Build list item
+                item = QListWidgetItem();
+                item.setToolTip(x)
+                item.setData(QtCore.Qt.UserRole, image)
+                item.setIcon(icon)
+
+                # Insert list item at appropriate place
+                if whom == 'openAct':                   # Append to list
+                    self.addItem(item)
+                elif whom == 'insertAct':               # Insert before selection
+                    self.insertItem(rows[0], item)
+                    rows[0] = rows[0] + 1
+                elif whom == 'replaceAct':              # Replace selection, then treat as insert
+                    self.takeItem(rows[0])
+                    self.insertItem(rows[0], item)
+                    if len(rows) > 1:
+                        rows = rows[1:]
+                    else:
+                        whom = 'insertAct'
+                        rows[0] = rows[0] + 1
+
+#            # Update progress bar
+#            pbar.setValue(pbar.value() + 1)
+#
+#        # Cleanup status bar
+#        self.statusbar.removeWidget(pbar)
+#        self.statusLabel.setText("Ready")
+
+        # (Re)number all the loaded pages
+        for x in range(self.count()):
+            self.item(x).setText(str(x+1))
+
+    def writeFiles(self):
+        # Determine button pressed
+        whom = self.sender().objectName()
+        if whom == "saveAct":
+            txt = 'Save Files'
+        elif whom == "saveAsAct":
+            txt = 'Save As Files'
+        else:
+            txt = 'Create TIFF'
+        print(txt + " not implemented")
+
--- tiffany.py
+++ tiffany.py
@@ -10,6 +10,7 @@ from PyQt5.QtGui import QImage, QPixmap, QPalette, QPainter
 
 from mainWin import Ui_MainWindow
 from PopupQToolButton import PopupQToolButton
+from Bookmarks import Bookmarks
 
 class Window(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
@@ -41,12 +42,12 @@ class Window(QMainWindow, Ui_MainWindow):
         self.zoomOutAct.setText(_translate("MainWindow", "Zoom\n&Out (25%)"))
 
     def connectSignalsSlots(self):
-        self.openAct.triggered.connect(self.readFiles)
-        self.insertAct.triggered.connect(self.readFiles)
-        self.replaceAct.triggered.connect(self.readFiles)
-        self.saveAct.triggered.connect(self.writeFiles)
-        self.saveAsAct.triggered.connect(self.writeFiles)
-        self.createTIFFAct.triggered.connect(self.writeFiles)
+        self.openAct.triggered.connect(self.listWidget.readFiles)
+        self.insertAct.triggered.connect(self.listWidget.readFiles)
+        self.replaceAct.triggered.connect(self.listWidget.readFiles)
+        self.saveAct.triggered.connect(self.listWidget.writeFiles)
+        self.saveAsAct.triggered.connect(self.listWidget.writeFiles)
+        self.createTIFFAct.triggered.connect(self.listWidget.writeFiles)
         self.exitAct.triggered.connect(self.close)
         self.zoomInAct.triggered.connect(self.zoomIn)
         self.zoomOutAct.triggered.connect(self.zoomOut)
@@ -96,97 +97,6 @@ class Window(QMainWindow, Ui_MainWindow):
         self.zoomToolButton.setMenu(self.zoomMenu)
         self.zoomToolButton.setDefaultAction(self.fitToWindowAct)
         self.toolBar.addWidget(self.zoomToolButton)
-
-    def readFiles(self):
-        # Determine button pressed
-        whom = self.sender().objectName()
-        if whom == "insertAct":
-            txt = 'Insert Files'
-        elif whom == "replaceAct":
-            txt = 'Replace Files'
-        else:
-            txt = 'Open Files'
-
-        # Popup file dialog
-        options = QFileDialog.Options()
-        fileNames,_ = QFileDialog.getOpenFileNames(self, txt, '', 'Images (*.png)', options=options)
-        if len(fileNames) == 0:
-            return
-
-        # For replace or insert, get list of items
-        rows = []
-        if (whom == 'insertAct') or (whom == 'replaceAct'):
-            for x in self.listWidget.selectedItems():
-                rows.append(int(x.text()) - 1)
-            rows.sort()
-            if len(rows) == 0:
-                QMessageBox.information(self, txt, "Insertion point must be selected")
-                return
-
-        # Add progress to status bar
-        self.statusLabel.setText("Reading...")
-        pbar = QProgressBar()
-        pbar.setRange(0, len(fileNames))
-        pbar.setValue(0)
-        self.statusbar.addWidget(pbar)
-
-        # Open each file in turn and add to listWidget
-        for x in fileNames:
-            image = QImage(x)
-            if image.isNull():
-                QMessageBox.information(self, "Tiffany", "Cannot load %s." % x)
-            else:
-                # Convert from indexed to grayscale
-                if image.format() == QImage.Format_Indexed8:
-                    image = image.convertToFormat(QImage.Format_Grayscale8)
-
-                # Make icon
-                qimg = image.scaled(100, 100, QtCore.Qt.KeepAspectRatio, QtCore.Qt.SmoothTransformation)
-                qpix = QPixmap.fromImage(qimg)
-                icon = QtGui.QIcon(qpix)
-
-                # Build list item
-                item = QListWidgetItem();
-                item.setToolTip(x)
-                item.setData(QtCore.Qt.UserRole, image)
-                item.setIcon(icon)
-
-                # Insert list item at appropriate place
-                if whom == 'openAct':                   # Append to list
-                    self.listWidget.addItem(item)
-                elif whom == 'insertAct':               # Insert before selection
-                    self.listWidget.insertItem(rows[0], item)
-                    rows[0] = rows[0] + 1
-                elif whom == 'replaceAct':              # Replace selection, then treat as insert
-                    self.listWidget.takeItem(rows[0])
-                    self.listWidget.insertItem(rows[0], item)
-                    if len(rows) > 1:
-                        rows = rows[1:]
-                    else:
-                        whom = 'insertAct'
-                        rows[0] = rows[0] + 1
-
-            # Update progress bar
-            pbar.setValue(pbar.value() + 1)
-
-        # Cleanup status bar
-        self.statusbar.removeWidget(pbar)
-        self.statusLabel.setText("Ready")
-
-        # (Re)number all the loaded pages
-        for x in range(self.listWidget.count()):
-            self.listWidget.item(x).setText(str(x+1))
-
-    def writeFiles(self):
-        # Determine button pressed
-        whom = self.sender().objectName()
-        if whom == "saveAct":
-            txt = 'Save Files'
-        elif whom == "saveAsAct":
-            txt = 'Save As Files'
-        else:
-            txt = 'Create TIFF'
-        print(txt + " not implemented")
 
     def imageSelected(self, curr, prev):
         self.currImage = curr.data(QtCore.Qt.UserRole)
