492a73e21a6e1b1a356cdf23962d877799fb0fa5
CRWolff <27215580+crwolff@users.noreply.github.com>
2022-02-22 09:37:45 -0700

    Add rotate and delete

--- Bookmarks.py
+++ Bookmarks.py
@@ -2,7 +2,7 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QFileDialog, QMessageBox, QListWidget, QListWidgetItem
-from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtGui import QImage, QPixmap, QTransform
 
 class Bookmarks(QListWidget):
     progressSig = QtCore.pyqtSignal(str, int)
@@ -46,25 +46,20 @@ class Bookmarks(QListWidget):
 
         # Open each file in turn and add to listWidget
         progress = 1
-        for x in fileNames:
-            image = QImage(x)
+        for fname in fileNames:
+            image = QImage(fname)
             if image.isNull():
-                QMessageBox.information(self, "Tiffany", "Cannot load %s." % x)
+                QMessageBox.information(self, "Tiffany", "Cannot load %s." % fname)
             else:
                 # Convert from indexed to grayscale
                 if image.format() == QImage.Format_Indexed8:
                     image = image.convertToFormat(QImage.Format_Grayscale8)
 
-                # Make icon
-                qimg = image.scaled(100, 100, QtCore.Qt.KeepAspectRatio, QtCore.Qt.SmoothTransformation)
-                qpix = QPixmap.fromImage(qimg)
-                icon = QtGui.QIcon(qpix)
-
-                # Build list item
+                # Create the list item
                 item = QListWidgetItem();
-                item.setToolTip(x)
+                item.setToolTip(fname)
                 item.setData(QtCore.Qt.UserRole, image)
-                item.setIcon(icon)
+                item.setIcon(self.makeIcon(image))
 
                 # Insert list item at appropriate place
                 if whom == 'openAct':                   # Append to list
@@ -87,8 +82,8 @@ class Bookmarks(QListWidget):
         self.progressSig.emit("", -1)
 
         # (Re)number all the loaded pages
-        for x in range(self.count()):
-            self.item(x).setText(str(x+1))
+        for idx in range(self.count()):
+            self.item(idx).setText(str(idx+1))
 
         # Force re-layout
         self.setSpacing(0)
@@ -104,3 +99,65 @@ class Bookmarks(QListWidget):
             txt = 'Create TIFF'
         print(txt + " not implemented")
 
+    def rotateSelection(self):
+        # Determine button pressed
+        whom = self.sender().objectName()
+        if whom == "rotateCWAct":
+            tmat = QTransform().rotate(90.0)
+        elif whom == "rotateCCWAct":
+            tmat = QTransform().rotate(270.0)
+        else:
+            tmat = QTransform().rotate(180.0)
+
+        # Make a list of all selected items
+        rows = []
+        for x in self.selectedItems():
+            rows.append(int(x.text()) - 1)
+
+        # Update items in place
+        if len(rows) > 0:
+            for idx in rows:
+                oldImage = self.item(idx).data(QtCore.Qt.UserRole)
+                rotImage = oldImage.transformed(tmat, QtCore.Qt.SmoothTransformation)
+
+                # Update item
+                self.item(idx).setData(QtCore.Qt.UserRole, rotImage)
+                self.item(idx).setIcon(self.makeIcon(rotImage))
+
+        # Signal redraw
+        self.currentItemChanged.emit(self.currentItem(), None)
+
+        # Force re-layout
+        self.setSpacing(0)
+
+    def deleteSelection(self):
+        # Make a list of all selected items
+        rows = []
+        for x in self.selectedItems():
+            rows.append(int(x.text()) - 1)
+        rows.sort(reverse=True)
+
+        # Remove from list
+        for idx in rows:
+            self.takeItem(idx)
+
+        # (Re)number all the loaded pages
+        for idx in range(self.count()):
+            self.item(idx).setText(str(idx+1))
+
+        # Select something near the deleted items
+        if self.count() == 0:
+            self.setCurrentItem(None)
+        elif rows[-1] < self.count():
+            self.setCurrentItem(self.item(rows[-1]))
+        else:
+            self.setCurrentItem(self.item(self.count()-1))
+
+        # Force re-layout
+        self.setSpacing(0)
+
+    def makeIcon(self,image):
+        qimg = image.scaled(100, 100, QtCore.Qt.KeepAspectRatio, QtCore.Qt.SmoothTransformation)
+        qpix = QPixmap.fromImage(qimg)
+        icon = QtGui.QIcon(qpix)
+        return icon
