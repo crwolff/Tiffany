dac5f144037abd8c0c880ccdf42bbaa79e80fd40
CRWolff <27215580+crwolff@users.noreply.github.com>
2022-10-22 13:37:13 -0600

    Rename despeckle spinbox

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -183,7 +183,7 @@ void MainWindow::buildToolBar()
     threshold = ui->toolBar->addWidget(thresholdWidget);
     threshold->setVisible(false);
     ui->toolBar->addAction(ui->despeckleAct);
-    despeckleWidget = new SpinWidget(1, 100, 50, "Despeckle\nArea", ui->toolBar);
+    despeckleWidget = new SpinWidget(1, 100, 50, "Maximum\nBlob Size", ui->toolBar);
     despeckle = ui->toolBar->addWidget(despeckleWidget);
     despeckle->setVisible(false);
 
