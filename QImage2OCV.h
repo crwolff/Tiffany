629f1fbee32a7f9442102a3402c60eb1734e0f88
CRWolff <27215580+crwolff@users.noreply.github.com>
2023-03-01 18:55:07 -0700

    Fence around QImage2OCV

--- QImage2OCV.h
+++ QImage2OCV.h
@@ -1,3 +1,8 @@
+// QImage2OCV.h
+
+#ifndef QIMAGE2OCV_H
+#define QIMAGE2OCV_H
+
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/imgproc/types_c.h>
 #include <QDebug>
@@ -6,3 +11,4 @@
 cv::Mat QImage2OCV(QImage &img);
 QImage OCV2QImage(cv::Mat &inMat);
 QImage OCV2QImage(cv::Mat &inMat, QImage &ref);
+#endif
